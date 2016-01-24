22da87b6b5947d36de48ae1461737fed96da0c29
subbadesu <subba_desu@yahoo.com>
2016-01-23 19:33:38 -0500

    Service Changes

--- pnl/src/main/java/com/infocube/risk/services/PortfolioService.java
+++ pnl/src/main/java/com/infocube/risk/services/PortfolioService.java
@@ -8,58 +8,53 @@ import com.datastax.driver.core.Row;
 import com.datastax.driver.core.Session;
 import com.datastax.driver.core.querybuilder.QueryBuilder;
 import com.datastax.driver.core.querybuilder.Select;
+import com.datastax.driver.mapping.Mapper;
+import com.datastax.driver.mapping.MappingManager;
+import com.datastax.driver.mapping.Result;
 import com.infocube.risk.db.ConnectionProperties;
 import com.infocube.risk.db.DbConnection;
 import com.infocube.risk.db.ObjectStore;
 import com.infocube.risk.entities.Instrument;
 import com.infocube.risk.entities.Portfolio;
+import com.infocube.risk.entities.Trade;
 
 public class PortfolioService implements Service {
 
-//	private static final String PORTFOLIO_TABLE_NAME = "inforisk_portfolio";
+	// private static final String PORTFOLIO_TABLE_NAME = "inforisk_portfolio";
 	private static final String DBSCHEMA_NAME = "infocube";
 	private static final String DBHOST_NAME = "localhost";
 
-	public  ArrayList<Portfolio>  getPortfolios()
-	{
-		ArrayList<Portfolio>  portfolioList = new ArrayList<Portfolio>();
-		  DbConnection dbConnection = new DbConnection(new ConnectionProperties());
-		  
-		  try {
-			  ObjectStore<Portfolio> portfolioStore = dbConnection.getObjectStore(Portfolio.class);
-		      Portfolio p = portfolioStore.get(1);
-		      portfolioList.add(p);
-		  }
-		  catch(Exception e) {
-			  e.printStackTrace();
-		  }
-	      dbConnection.close();
-		
-	      return portfolioList;
+	public ArrayList<Portfolio> getPortfolio(int portfolioID) {
+		ArrayList<Portfolio> portfolioList = new ArrayList<Portfolio>();
+		DbConnection dbConnection = new DbConnection(new ConnectionProperties());
+
+		try {
+			ObjectStore<Portfolio> portfolioStore = dbConnection.getObjectStore(Portfolio.class);
+			Portfolio p = portfolioStore.get(1);
+			portfolioList.add(p);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		dbConnection.close();
+
+		return portfolioList;
 	}
+
+	public ArrayList<Portfolio> getPortfolios() {
+		ArrayList<Portfolio> portfolioList = new ArrayList<Portfolio>();
+		DbConnection dbConnection = new DbConnection(new ConnectionProperties());
+		Session session = dbConnection.getSession();
+		MappingManager manager = new MappingManager(session);
+		ResultSet results = session.execute("SELECT * FROM inforisk_portfolio");
+		Mapper<Portfolio> mapper = manager.mapper(Portfolio.class);
+		Result<Portfolio> portfolios = mapper.map(results);
+		for (Portfolio u : portfolios) {
+			portfolioList.add(u);
+		}
+		dbConnection.close();
+		return portfolioList;
+
+	}
+
 	
-	
-	public  ArrayList<Portfolio>  getPortfoliostmp()
-	{
-	      ArrayList<Portfolio>  portfolioList = new ArrayList<Portfolio>();
-		  Portfolio p = new Portfolio(1,"Equity");
-	      portfolioList.add(p);
-		
-	      return portfolioList;
-	        
-		
-	}
-
-	public ArrayList<Portfolio> getPortfolio() {
-		Portfolio p1 = new Portfolio(1, "Equities");
-		Portfolio p2 = new Portfolio(1, "Credit");
-		Portfolio p3 = new Portfolio(1, "Rates");
-		ArrayList<Portfolio> p = new ArrayList<Portfolio>();
-		p.add(p1);
-		p.add(p2);
-		p.add(p3);
-
-		return p;
-
-	}
 }
--- pnl/src/main/java/com/infocube/risk/services/TradeService.java
+++ pnl/src/main/java/com/infocube/risk/services/TradeService.java
@@ -1,5 +1,32 @@
 package com.infocube.risk.services;
 
-public class TradeService  implements Service {
+import java.util.ArrayList;
 
+import com.datastax.driver.core.ResultSet;
+import com.datastax.driver.core.Session;
+import com.datastax.driver.mapping.Mapper;
+import com.datastax.driver.mapping.MappingManager;
+import com.datastax.driver.mapping.Result;
+import com.infocube.risk.db.ConnectionProperties;
+import com.infocube.risk.db.DbConnection;
+import com.infocube.risk.entities.Trade;
+
+public class TradeService implements Service {
+	private static final String DBSCHEMA_NAME = "infocube";
+	private static final String DBHOST_NAME = "localhost";
+
+	public ArrayList<Trade> getTrades(int portfolioId) {
+		ArrayList<Trade> tradeList = new ArrayList<Trade>();
+		DbConnection dbConnection = new DbConnection(new ConnectionProperties());
+		Session session = dbConnection.getSession();
+		MappingManager manager = new MappingManager(session);
+		ResultSet results = session.execute("SELECT * FROM inforisk_trade");
+		Mapper<Trade> mapper = manager.mapper(Trade.class);
+		Result<Trade> trades = mapper.map(results);
+		for (Trade t : trades) {
+			tradeList.add(t);
+		}
+		dbConnection.close();
+		return tradeList;
+	}
 }
